9325fd76d474074baa81f057865c01c2bed5f21b
Allan Ramos <allangrds@gmail.com>
2019-03-10 19:30:06 -0300

    feat(Auth): add JWT authentication

--- project/routes/api.php
+++ project/routes/api.php
@@ -2,17 +2,23 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::prefix('users')->group(function () {
+    Route::post('/', 'UserController@store');
+});
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('auth')->group(function () {
+    Route::post('/login', 'AuthController@login');
+});
+
+// Route::middleware('auth:api')->group(function () { -> alternativa
+Route::middleware('jwt.auth')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::prefix('auth')->group(function () {
+        Route::get('/refresh', 'AuthController@refresh');
+        //Route::middleware('jwt.refresh')->get('/token/refresh', 'AuthController@refresh'); -> alternativa
+        Route::post('/logout', 'AuthController@logout');
+    });
 });
